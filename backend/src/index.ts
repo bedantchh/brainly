36ff19d60f408b9129c3306005d0413be2888e1a
Bedant Chhetri <bedantchhetri7@gmail.com>
2025-05-10 08:36:28 +0530

    initialize

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import express  from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/api/v1/signup",(req,res)=>{
+
+})
+app.post("/api/v1/signin",(req,res)=>{
+
+})
+app.post("/api/v1/content",(req,res)=>{
+
+})
+app.get("/api/v1/content",(req,res)=>{
+
+})
+app.delete("/api/v1/delete",(req,res)=>{
+    
+})
+
+app.listen(3000)
